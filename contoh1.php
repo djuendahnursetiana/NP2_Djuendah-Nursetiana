20747618fc9b492c8137526f905101561406e83b
djuendahnursetiana <juendah@gmail.com>
2021-09-20 21:16:28 +0700

    Menambahkan contoh1.php

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_controller
+{
+	puplic function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo "Nama Saya Djuendah Nursetiana
+			  saya tinggal di daerah kotabumi
+			  olah raga yang saya sukai adalah Bulutangkis";
+	}
+}
